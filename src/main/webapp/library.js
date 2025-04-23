9ea1ca22b07a617db2a24009afd252e84dbc55b4
shwetha-s-codes <shwethakodimar29@gmail.com>
2025-04-23 10:23:34 +0530

    Basic Front end is completed

--- src/main/webapp/library.js
+++ src/main/webapp/library.js
@@ -13,7 +13,8 @@ function addToTable()
 	const coverimage=document.getElementById("coverimage");
 	const image=coverimage.files[0];
 	const imageUrl=URL.createObjectURL(image);
-	const revrate=document.getElementById("revrate").value;
+	const author=document.getElementById("author").value;
+	const rating=document.getElementById("rating").value;
 	
 			const table=document.getElementById("infoTable").getElementsByTagName('tbody')[0];
 			const newRow=table.insertRow();
@@ -22,14 +23,39 @@ function addToTable()
 			const cell2=newRow.insertCell(1);
 			const cell3=newRow.insertCell(2);
 			const cell4=newRow.insertCell(3);
+			const cell5=newRow.insertCell(4);
 			
 			cell1.textContent=bookname;
+			
 			const imageinput=document.createElement("img");
 			imageinput.src=imageUrl;
 			imageinput.width=100;
 			imageinput.height=150;
 			cell2.appendChild(imageinput)
-			cell3.textContent=revrate;
+			
+			cell3.textContent=author;
+			cell4.textContent=rating;
+			
+			const editbtn=document.createElement("button");
+			editbtn.textContent="Edit";
+			editbtn.className="edit-btn";
+			editbtn.style.border="2px solid black";
+			editbtn.style.marginRight="4px";
+			editbtn.style.marginLeft="2px";
+						
+			
+			const delbtn=document.createElement("button");
+			delbtn.textContent="Delete";
+			delbtn.className="del-btn";
+			delbtn.style.border="2px solid black";
+			
+			
+			
+			
+							
+			
+			cell5.appendChild(editbtn);
+			cell5.appendChild(delbtn);
 			
 			
 	document.getElementById("myform").reset();
